b4001f4e402864739d7877fd9033e70174c25fce
Mohammed-Aswath <mohammed.aswath07@gmail.com>
2025-07-12 12:19:06 +0530

    resolving CORS error

--- server/index.js
+++ server/index.js
@@ -6,7 +6,10 @@ require('dotenv').config();
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+  origin: "*", // Allow all during testing (not recommended in prod)
+  methods: ["GET", "POST", "PUT", "DELETE"],
+}));
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI).then(() => {
